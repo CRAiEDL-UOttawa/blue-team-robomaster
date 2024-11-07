14adc5c8460ae644ffb6e05b311d4e2084cfe13c
Vanisha Bagga <vanishabagga@gmail.com>
2024-11-07 17:49:00 -0500

    extra shooting method

--- game_functions/shooting.py
+++ game_functions/shooting.py
@@ -1,5 +1,18 @@
-# shoot lazer out once
-def shoot_one_lazer():
+def shoot_lazer(x): # x -> amount of times you want the countdown sound to play
+    for count in range(x):
+        # turn gun light on
+        led_ctrl.gun_led_on()
+
+        # fire lazer
+        ir_blaster_ctrl.fire_once()
+
+        # turn gun light off
+        led_ctrl.gun_led_off()
+
+        # play shoot sound once (audio.py)
+        shoot_sound(1)
+
+def shoot_one_lazer(): 
     # turn gun light on
     led_ctrl.gun_led_on()
 
