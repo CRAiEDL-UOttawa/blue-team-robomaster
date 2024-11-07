231caf5402edeb8488e09bb2d51b705a2ee4d082
YasinUottawa <yasinelmi100@gmail.com>
2024-11-07 18:37:37 -0500

    started game loop logic

--- game.py
+++ game.py
@@ -4,33 +4,21 @@ def start():
 	print('game start')
 	# INTRO SCENE
 
+	gamefunction = [detect_vmarker,detect_gesture]
 
 
 	# GAME LOOP
-	gameLoopCounter = -1
+	
+	
+	for i in range(0,10):
+	
+		SimonSays = random.randint(0,1)
+		if(SimonSays):
+			media_ctrl.play_sound(rm_define.media_custom_audio_SIMONSAYS, wait_for_complete_flag=True)
+		
 
-	while True:
-
-		# Says simon says each action in a loop
-		if(gameLoopCounter<0):
-			media_ctrl.play_sound("SIMON SAYS AUDIO",wait_for_complete_flag=True)
-			gameLoopCounter+=1
-
-		# CODE TO RANDOMLY SELECT
-		selected_audio = []
-		selected_action = []
-		media_ctrl.play_sound(selected_audio)
-
-		# Start the timer
-		tools.timer_ctrl(rm_define.timer_start)
-
-		if vision_ctrl.check_condition(selected_action):			
-			media_ctrl.play_sound(rm_define.media_sound_recognize_success, wait_for_complete=True)
-			gameLoopCounter-=1
-		else:
-			if tools.timer_current() > 10:
-				shoot_one_lazer()
-				gameLoopCounter-=1		
+		#random.choice(gamefunction)
+		
 
 
 	# EXIT SCENE
