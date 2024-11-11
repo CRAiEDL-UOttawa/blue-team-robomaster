d97695314becd7e14d46220cb17e90a0cd6fd387
YasinUottawa <yasinelmi100@gmail.com>
2024-11-11 16:37:15 -0500

    added colors

--- game_functions/lights.py
+++ game_functions/lights.py
@@ -15,7 +15,7 @@ RGB = {
 }
 
 # using colours defined in dictionary
-def set_led_color(top_color, bottom_color):
+def set_led_color(top_color, bottom_color, effect):
     # get RGB values for colors
     top_rgb = RGB.get(top_color)
     bottom_rgb = RGB.get(bottom_color)
@@ -27,24 +27,24 @@ def set_led_color(top_color, bottom_color):
         raise ValueError(f"Bottom color '{bottom_color}' not found.")
     
     # set the top and bottom LEDs 
-    led_ctrl.set_top_led(rm_define.armor_top_all, top_rgb[0], top_rgb[1], top_rgb[2], rm_define.effect_always_on)
-    led_ctrl.set_bottom_led(rm_define.armor_bottom_all, bottom_rgb[0], bottom_rgb[1], bottom_rgb[2], rm_define.effect_always_on)
+    led_ctrl.set_top_led(rm_define.armor_top_all, top_rgb[0], top_rgb[1], top_rgb[2], effect)
+    led_ctrl.set_bottom_led(rm_define.armor_bottom_all, bottom_rgb[0], bottom_rgb[1], bottom_rgb[2], effect)
 
 
 # using user's 1 array
-def set_led_color(rgb_values):
+def set_led_color(rgb_values, effect):
     # check array has three values
     if len(rgb_values) != 3:
         raise ValueError("Array should contain exactly three elements: [x1, x2, x3]")
     
     x1, x2, x3 = rgb_values
 
-    led_ctrl.set_top_led(rm_define.armor_top_all, x1, x2, x3, rm_define.effect_always_on)
-    led_ctrl.set_bottom_led(rm_define.armor_bottom_all, x1, x2, x3, rm_define.effect_always_on)
+    led_ctrl.set_top_led(rm_define.armor_top_all, x1, x2, x3, effect)
+    led_ctrl.set_bottom_led(rm_define.armor_bottom_all, x1, x2, x3, effect)
 
 
 # using user's 2 arrays
-def set_led_colors_dif(top_rgb, bottom_rgb):
+def set_led_colors_dif(top_rgb, bottom_rgb, effect):
     # check each array has three values
     if len(top_rgb) != 3 or len(bottom_rgb) != 3:
         raise ValueError("Each array should contain exactly three elements: [x1, x2, x3]")
@@ -52,5 +52,5 @@ def set_led_colors_dif(top_rgb, bottom_rgb):
     top_x1, top_x2, top_x3 = top_rgb
     bottom_x1, bottom_x2, bottom_x3 = bottom_rgb
     
-    led_ctrl.set_top_led(rm_define.armor_top_all, top_x1, top_x2, top_x3, rm_define.effect_always_on)
-    led_ctrl.set_bottom_led(rm_define.armor_bottom_all, bottom_x1, bottom_x2, bottom_x3, rm_define.effect_always_on)
+    led_ctrl.set_top_led(rm_define.armor_top_all, top_x1, top_x2, top_x3, effect)
+    led_ctrl.set_bottom_led(rm_define.armor_bottom_all, bottom_x1, bottom_x2, bottom_x3, effect)
