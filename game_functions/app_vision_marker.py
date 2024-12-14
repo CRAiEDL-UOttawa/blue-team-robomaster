975f963124430aa5ae69206e09992d8d63119e06
Vanisha Bagga <vanishabagga@gmail.com>
2024-12-13 22:16:03 -0500

    more comments

--- game_functions/app_vision_marker.py
+++ game_functions/app_vision_marker.py
@@ -1,6 +1,6 @@
+# Code done to test vision markers
 
-import random
-
+# Dictionary that holds vision marker object and corresponding audio (command that says to pick up corresponding card)
 markers_num_dict = {
     rm_define.cond_recognized_marker_number_two: rm_define.media_custom_audio_4, 
     rm_define.cond_recognized_marker_number_three: rm_define.media_custom_audio_0, 
@@ -9,16 +9,16 @@ markers_num_dict = {
 
 def shoot_lazer(x): # x -> amount of times you want to shoot
     for count in range(x):
-        # turn gun light on
+        # Turn gun light on
         led_ctrl.gun_led_on()
 
-        # fire lazer
+        # Fire lazer
         ir_blaster_ctrl.fire_once()
 
-        # turn gun light off
+        # Turn gun light off
         led_ctrl.gun_led_off()
 
-        # play shoot sound once (audio.py)
+        # Play shoot sound once 
         shoot_sound(1)
 
 def success_sound(x): # x -> amount of times you want the success sound to play
@@ -26,36 +26,41 @@ def success_sound(x): # x -> amount of times you want the success sound to play
             media_ctrl.play_sound(rm_define.media_sound_recognize_success, wait_for_complete=True)
 
 def start():
-    # vision detection enabled
+    # Vision detection enabled
     led_ctrl.set_bottom_led(rm_define.armor_bottom_all, 255, 50, 0, rm_define.effect_always_on)
     vision_ctrl.enable_detection(rm_define.vision_detection_marker)
 
-    # max distance 
-    vision_ctrl.set_marker_detection_distance(1)
+    # Max distance 
+    vision_ctrl.set_marker_detection_distance(3)
 
-    # constantly running
+    # Constantly running
     while True: 
-        # select one key in dict
+        # Select one key in dict
         marker = random.choice(list(markers_num_dict.keys()))
 
+        # Play corresponding audio
         audio = markers_num_dict[marker]
         media_ctrl.play_sound(audio, wait_for_complete_flag=True)
 
-        # timer
+        # Timer starts
         tools.timer_ctrl(rm_define.timer_start)
 
         win = False
 
+        # Player has 10 seconds to pick up vision card
         while tools.timer_current() < 10:
             
+            # If the right vision marker is detected
             if vision_ctrl.check_condition(marker):
-                # led light changes to orange
+                # LED light changes to orange
                 led_ctrl.set_bottom_led(rm_define.armor_bottom_all, 161, 255, 69, rm_define.effect_always_on)
-                success_sound(1)
+                success_sound(1) # Play success sound
                 win = True
              
-            if win == False:
+            if win == False: # Player did not pick up the right card/any card or 10 seconds are up and right card has not been shown
+                # LED light changes to red
                 led_ctrl.set_bottom_led(rm_define.armor_bottom_all, 255, 0, 0, rm_define.effect_always_on)
-                shoot_lazer(5)
+                shoot_lazer(5) # Shoot 5 times
 
+        # Reset timer
         tools.timer_ctrl(rm_define.timer_reset) 
--- game_functions/audio.py
+++ game_functions/audio.py
@@ -1,4 +1,3 @@
-
 def countdown_sound(x): # x -> amount of times you want the countdown sound to play
     for count in range(x):
         media_ctrl.play_sound(rm_define.media_sound_count_down, wait_for_complete=True)
@@ -29,17 +28,22 @@ def happy_sound(x): # x -> amount of times you want the happy sound to play
       media_ctrl.play_sound(rm_define.media_sound_solmization_2B)
       media_ctrl.play_sound(rm_define.media_sound_solmization_3C)
 
-# use while waiting for players to do action
-def waiting_sound_l1(x):
-      media_ctrl.play_sound(rm_define.media_sound_solmization_1A)
-
-def waiting_sound_l2(x):
-      media_ctrl.play_sound(rm_define.media_sound_solmization_1GSharp)
-
-def waiting_sound_l3(x):
-      media_ctrl.play_sound(rm_define.media_sound_solmization_1F)
-
 def angry_sound(x): # x -> amount of times you want the sad sound to play
       media_ctrl.play_sound(rm_define.media_sound_solmization_1E)
       media_ctrl.play_sound(rm_define.media_sound_solmization_1B)
-      ## need to make this more evil
+
+# Different level sounds in our game
+def l1():
+      media_ctrl.play_sound(rm_define.media_sound_solmization_2C, wait_for_complete=True)
+      media_ctrl.play_sound(rm_define.media_sound_solmization_2CSharp, wait_for_complete=True)
+      media_ctrl.play_sound(rm_define.media_sound_solmization_2C, wait_for_complete=True)
+
+def l2():
+      media_ctrl.play_sound(rm_define.media_sound_solmization_1G, wait_for_complete=True)
+      media_ctrl.play_sound(rm_define.media_sound_solmization_2CSharp, wait_for_complete=True)
+      media_ctrl.play_sound(rm_define.media_sound_solmization_1G, wait_for_complete=True)
+
+def l3():
+      media_ctrl.play_sound(rm_define.media_sound_solmization_1C, wait_for_complete=True)
+      media_ctrl.play_sound(rm_define.media_sound_solmization_2CSharp, wait_for_complete=True)
+      media_ctrl.play_sound(rm_define.media_sound_solmization_1C, wait_for_complete=True)
